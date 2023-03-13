edf5f88c3643c4d9591ac45551b4211f3051ec99
Flávio Macario de Souza <flavio17_canarana@hotmail.com>
2023-03-13 15:51:33 -0300

    Carregamento dos arquivos base do projeto
    Upload dos arquivos principais do meu portfólio...

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/* typing animation */
+var typed = new Typed(".typing", {
+	strings:[" ","Desenvolvedor Full Stack", "Web developer",
+	 "Desenvolvedor Front End",
+	 "Desenvolvedor Full Stack", "Programador"],
+	 typeSpeed: 100,
+	 BackSpeed: 60,
+	 loop: true
+})
